4242254787162b240f499697a89dea56e2e31da7
naveed <mohdnaveed96@gmail.com>
2024-02-19 21:46:43 -0600

    a-star working

--- a_star.py
+++ a_star.py
@@ -14,7 +14,7 @@ COL = 10
 
 class node:
 
-    def init(self):
+    def __init__(self):
 
         self.parent_i = 0 # row index of parent
         self.parent_j = 0 # col index of parent
@@ -40,6 +40,28 @@ def is_destination(node, dest):
 
     return (node[0] == dest[0] and node[1] == dest[1])
 
+def trace_path(src, dest, node_details):
+
+    i = dest[0]
+    j = dest[1]
+    path = []
+    path.append(dest)
+
+    while not (i == src[0] and j == src[1]):
+
+        new_i = node_details[i][j].parent_i
+        new_j = node_details[i][j].parent_j
+        
+        path.append([new_i, new_j])
+        i = new_i
+        j = new_j
+
+    path.reverse()
+    for i in path:
+        print("->", i, end=" ")
+
+
+
 def a_star_search(grid, src, dest):
 
     #check if src and dest are valid
@@ -54,11 +76,10 @@ def a_star_search(grid, src, dest):
 
     #initialize closed list
     closed_list = [[False for _ in range(COL)] for _ in range(ROW)]
-    print(closed_list)
 
     #initialize every node.
-    node_details = [[node() for _ in range(COL)] for _ in range()]
-
+    node_details = [[node() for _ in range(COL)] for _ in range(ROW)]
+    
     #initialize the start node details
     node_details[src[0]][src[1]].f = 0
     node_details[src[0]][src[1]].g = 0 
@@ -79,9 +100,9 @@ def a_star_search(grid, src, dest):
 
         cur_node = heapq.heappop(open_list)
 
-        i = cur_node[0] # cur row
-        j = cur_node[1]
-
+        i = cur_node[1] # cur row
+        j = cur_node[2]
+        #print("node = ", i, j, "parent = ", node_details[i][j].parent_i, node_details[i][j].parent_j)
         closed_list[i][j] = True
 
         # directions to check for successors.
@@ -90,18 +111,42 @@ def a_star_search(grid, src, dest):
         for dir in directions:
             new_i = i + dir[0]
             new_j = j + dir[1]
-
+            
+            
             #check if successor is valid
             if (is_valid(new_i, new_j) and is_unblocked(grid, new_i, new_j)):
 
-                if is_destination([new_i, new_j], dest)
+                if is_destination([new_i, new_j], dest):
+                    print('Destination reached. ')
+                    
+                    node_details[new_i][new_j].parent_i = i
+                    node_details[new_i][new_j].parent_j = j
+                    
+                    trace_path(src, dest, node_details)
+                    DEST_FOUND = True
 
-                node_details[new_i][new_j].g = node_details[i][j] + 1
-                node_details[new_i][new_j].h = cal_h([new_i, new_j], dest)
-                node_details[new_i][new_j].f = node_details[new_i][new_j].g + node_details[new_i][new_j].h
-                node_details[new_i][new_j].parent_i = i
-                node_details[new_i][new_j].parent_j = j
-                heapq.heappush()
+                    return
+                
+                else:
+                    
+                    #adding incremental cost. 
+                    g_new = node_details[i][j].g + 1
+                    node_details[new_i][new_j].h = cal_h([new_i, new_j], dest)
+                    f_new = g_new + node_details[new_i][new_j].h
+                    
+                    #checking if new cost is lower.
+                    if node_details[new_i][new_j].f > f_new:
+
+                        node_details[new_i][new_j].g = g_new
+                        node_details[new_i][new_j].f = f_new
+                    
+                        node_details[new_i][new_j].parent_i = i
+                        node_details[new_i][new_j].parent_j = j
+                        
+                        heapq.heappush(open_list, (f_new, new_i, new_j))
+
+    if not DEST_FOUND:
+        print('Destination not found.')
 
 
 if __name__ == "__main__":
